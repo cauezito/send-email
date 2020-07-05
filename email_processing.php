eea1c8a6110c3f8087d53c34027abc11053949e1
Cauê Santos <caah.sfc@live.com>
2020-07-05 15:57:21 -0300

    Criando e instanciando a classe Message

--- email_processing.php
+++ email_processing.php
@@ -1,9 +1,35 @@
 <?php
-$to = $_POST['to'];
-$subject = $_POST['subject'];
-$msg = $_POST['message'];
 
-echo $msg;
+    class Message{
+        private $to;
+        private $subject;
+        private $message;
 
+        public function __get($attr){
+            return $this->$attr;
+        }
+
+        public function __set($attr, $value){
+            $this->$attr = $value;
+        }
+
+        public function msgIsValid(){
+            if(empty($this->to) || empty($this->subject) || empty($this->message)){
+                return false;
+            }
+            return true;
+        }
+    }
+
+    $message = new Message();
+    $message->__set('to', $_POST['to']);
+    $message->__set('subject', $_POST['subject']);
+    $message->__set('message', $_POST['message']);
+
+    if($message->msgIsValid()){
+        echo 'Valid';
+    } else {
+        echo 'Invalid';
+    }
 ?>
 
